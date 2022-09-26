136e5f1c01d617ab8137c4532ab519f832d9e73f
Jacob <regerjacob@gmail.com>
2022-09-26 14:36:50 -0700

    Hello world and .gitignore

---
+++ main.rs
@@ -0,0 +1,6 @@
+fn main()
+{
+    println!("Hello, world!");
+    println!("My very first rust program, except for that one multithreaded one :)");
+    println!("Rust println! automatically adds newlines...");
+}
